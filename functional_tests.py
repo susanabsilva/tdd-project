69bc17e184a1c1be778eb3b00640164610d12258
Susana Borges <susana.bs058@gmail.com>
2026-09-06 19:30:04 -0300

    Utilizando Selenium para Testar Interações do Usuário

--- functional_tests.py
+++ functional_tests.py
@@ -1,29 +1,54 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 import unittest
 
-class NewVisitorTest(unittest.TestCase):  
 
-    def setUp(self):  
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
         self.browser = webdriver.Firefox()
 
-    def tearDown(self):  
+    def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrieve_it_later(self):  
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
         # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
         self.browser.get('http://localhost:8000')
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
 
+        # Ela verifica o cabeçalho da página
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
+        self.assertIn('To-Do', header_text)
+
         # Ela é convidada a entrar com um item TODO imediatamente
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Ela digita "Estudar testes funcionais" em uma caixa de texto
+        inputbox.send_keys('Estudar testes funcionais')
 
         # Quando ela aperta enter, a página atualiza, e mostra a lista
         # "1: Estudar testes funcionais" como um item da lista TODO
-        
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+
+        self.assertTrue(
+            any(row.text == '1: Estudar testes funcionais' for row in rows)
+        )
+
         # Ainda existe uma caixa de texto convidando para adicionar outro item
+
         # Ela digita: "Estudar testes de unidade"
 
         # A página atualiza novamente, e agora mostra ambos os itens na sua lista
@@ -35,5 +60,6 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfeita, ela vai dormir
 
-if __name__ == '__main__':  
+
+if __name__ == '__main__':
     unittest.main()
