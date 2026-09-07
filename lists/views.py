988596e8029e00520429d814fd0e152f6b21747d
Susana Borges <susana.bs058@gmail.com>
2026-09-06 22:45:54 -0300

    Não Salvar Items em Branco a cada Requisição

--- lists/views.py
+++ lists/views.py
@@ -3,11 +3,12 @@ from lists.models import Item
 
 
 def home_page(request):
+    new_item_text = ''
 
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
 
     return render(request, 'home.html', {
-        'new_item_text': item.text
+        'new_item_text': new_item_text
     })
