4a01fa0faf92a52cd170942f32a057fdc68923d3
Susana Borges <susana.bs058@gmail.com>
2026-09-06 21:55:58 -0300

    Melhorando e Expandindo o Teste Funcional

--- functional_tests.py
+++ functional_tests.py
@@ -15,18 +15,19 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_a_list_and_retrieve_it_later(self):
 
-        # Maria decidiu utilizar o novo app TODO. Ela entra em sua página principal:
+        # Maria decidiu utilizar o novo app TODO.
+        # Ela entra em sua página principal:
         self.browser.get('http://localhost:8000')
 
         # Ela nota que o título da página menciona TODO
         self.assertIn('To-Do', self.browser.title)
 
-        # Ela verifica o cabeçalho da página
         header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
         # Ela é convidada a entrar com um item TODO imediatamente
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
+
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -35,31 +36,49 @@ class NewVisitorTest(unittest.TestCase):
         # Ela digita "Estudar testes funcionais" em uma caixa de texto
         inputbox.send_keys('Estudar testes funcionais')
 
-        # Quando ela aperta enter, a página atualiza, e mostra a lista
-        # "1: Estudar testes funcionais" como um item da lista TODO
+        # Quando ela aperta enter, a página atualiza,
+        # e mostra a lista "1: Estudar testes funcionais"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element(By.ID, 'id_list_table')
         rows = table.find_elements(By.TAG_NAME, 'tr')
 
-        self.assertTrue(
-            any(row.text == '1: Estudar testes funcionais' for row in rows),
-            "New to-do item did not appear in table"
+        self.assertIn(
+            '1: Estudar testes funcionais',
+            [row.text for row in rows]
         )
 
-        # Ainda existe uma caixa de texto convidando para adicionar outro item
-
+        # Ainda existe uma caixa de texto convidando
+        # para adicionar outro item
         # Ela digita: "Estudar testes de unidade"
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
 
-        # A página atualiza novamente, e agora mostra ambos os itens na sua lista
+        inputbox.send_keys('Estudar testes de unidade')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-        # Maria se pergunta se o site vai lembrar da sua lista. Então, ela verifica que
-        # o site gerou uma URL única para ela -- existe uma explicação sobre essa feature
+        # A página atualiza novamente,
+        # e agora mostra ambos os itens na sua lista
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
 
-        # Ela visita a URL: a sua lista TODO ainda está armazenada
+        self.assertIn(
+            '1: Estudar testes funcionais',
+            [row.text for row in rows]
+        )
 
-        # Satisfeita, ela vai dormir
+        self.assertIn(
+            '2: Estudar testes de unidade',
+            [row.text for row in rows]
+        )
+
+        # Maria se pergunta se o site vai lembrar da sua lista.
+        # Então, ela verifica que o site gerou uma URL única para ela.
+        #
+        # Ela visita a URL: a sua lista TODO ainda está armazenada.
+        #
+        # Satisfeita, ela vai dormir.
 
 
 if __name__ == '__main__':
